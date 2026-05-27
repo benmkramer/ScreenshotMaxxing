f852c7e9ff3e3c597c49d807461db229baf4af8a
Ben Kramer <benmkramer@gmail.com>
2026-05-26 20:41:46 -0700

    Initial Commit

---
+++ ScreenshotMaxxingTests/ScreenshotMaxxingTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenshotMaxxingTests.swift
+//  ScreenshotMaxxingTests
+//
+//  Created by Ben Kramer on 5/26/26.
+//
+
+import Testing
+@testable import ScreenshotMaxxing
+
+struct ScreenshotMaxxingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
