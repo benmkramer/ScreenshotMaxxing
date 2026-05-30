97458f1deaa4dfbf024311c05b2d221694719626
Ben Kramer <benmkramer@gmail.com>
2026-05-30 10:10:59 -0700

    fix: clarify blur tool tooltip

--- ScreenshotMaxxing/Editor/EditorTool.swift
+++ ScreenshotMaxxing/Editor/EditorTool.swift
@@ -65,7 +65,7 @@ enum EditorTool: String, CaseIterable, Identifiable {
         case .select:
             "Select an annotation"
         case .blur:
-            "Draw a blur redaction"
+            "Draw a blur effect"
         case .pen:
             "Draw a solid line"
         case .highlighter:
